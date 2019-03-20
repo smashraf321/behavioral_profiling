628d7ad95ab6182c95364634bad7a7de0c641895
Ashraf Shaikh Mohammed <ashraf@Ashrafs-MacBook-Pro.local>
2019-03-19 23:18:59 -0500

    Uploader changes and added new py2 compatible file

--- firestore_parse_n_upload.py
+++ firestore_parse_n_upload.py
@@ -14,18 +14,29 @@ for curr_file in os.listdir(files_path):
             with open(files_path + curr_file,'r') as csv_file:
                 csv_reader = csv.reader(csv_file, delimiter = ',')
                 for rows in csv_reader:
-                    doc_name = 'row_' + str(rows[8])
-                    doc_ref = db.collection(u'first_log').document(doc_name)
+                    if len(rows[8]) == 1:
+                        doc_name = '00000' + str(rows[8])
+                    elif len(rows[8]) == 2:
+                        doc_name = '0000' + str(rows[8])
+                    elif len(rows[8]) == 3:
+                        doc_name = '000' + str(rows[8])
+                    elif len(rows[8]) == 4:
+                        doc_name = '00' + str(rows[8])
+                    elif len(rows[8]) == 5:
+                        doc_name = '0' + str(rows[8])
+                    else:
+                        doc_name = str(rows[8])
+                    doc_ref = db.collection(u'first_log_second_attempt').document(doc_name)
                     doc_ref.set({
-                        u'Time_stamp': rows[0],
-                        u'Engine_Temperature': rows[1],
-                        u'RPM': rows[2],
-                        u'Speed': rows[3],
-                        u'Throttle': rows[4],
+                        u'Time_stamp': float(rows[0]),
+                        u'Engine_Temperature': float(rows[1]),
+                        u'RPM': float(rows[2]),
+                        u'Speed': float(rows[3]),
+                        u'Throttle': float(rows[4]),
                         u'GPS_time': rows[7],
-                        u'Distance': rows[9]
+                        u'Distance': float(rows[9])
                     })
-                    if rows[5] != 0:
+                    if rows[5] != '0':
                         doc_ref.update({u'Lat': rows[5], u'Lon': rows[6]})
     except Exception as excptn:
         raise excptn
--- test.py
+++ test.py
@@ -1,15 +1,18 @@
 #import os
 
-print('test file')
+#print('test file')
 
-file_name = 'tester.csv'
+#file_name = 'tester.csv'
 
-outfile_name = open('current_file.txt','w+')
-print(file_name,file = outfile_name)
-outfile_name.close()
+#outfile_name = open('current_file.txt','w+')
+#print(file_name,file = outfile_name)
+#outfile_name.close()
 
 #outfile_name = open('current_file.txt','r')
 #filename = outfile_name.readline()
 #outfile_name.close()
 #to_be_removed = "rm -f " + filename.rstrip()
 #os.system(to_be_removed)
+
+strr = '1'
+print(len(strr))
