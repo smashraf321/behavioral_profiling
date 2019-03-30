ffce797ecf0b711183381a7fd01e6541e25af2ab
Ashraf Shaikh Mohammed <ashraf@Ashrafs-MacBook-Pro.local>
2019-03-30 16:24:12 -0500

    edited uploader

--- final_upload_tester.sh
+++ final_upload_tester.sh
@@ -8,10 +8,6 @@ NUPLDF=$(ls /home/pi/behavioral_profiling/Documents/logs/ | wc -l)
 
 if [[ "0" -eq $COUNT ]] && [[ "3" -eq $WIFI_STAT]] && [[ "0" -ne $NUPLDF ]]; then
 
-		./Dropbox-Uploader/dropbox_uploader.sh upload /home/pi/behavioral_profiling/Documents/logs/*.csv /
-		source /home/pi/firestore/bin/activate
-		python3 firestore_parse_n_upload.py
-		deactivate
-		rm -f /home/pi/behavioral_profiling/Documents/logs/*.csv
+		echo $HOME
 
 fi
