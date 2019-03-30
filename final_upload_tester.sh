d1784ba9c40ce450e88ee6852cedbf1f36061913
Ashraf Shaikh Mohammed <ashraf@Ashrafs-MacBook-Pro.local>
2019-03-30 16:31:11 -0500

    fixing uploader

--- final_upload_tester.sh
+++ final_upload_tester.sh
@@ -2,7 +2,7 @@
 
 # This is a script for upload process to dropbox and firestore
 
-COUNT=$(iwconfig 2>&1 | grep ESSID:off/any | wc -l)
+COUNT=$(iwconfig 2>&1 | grep -q ESSID:off/any | wc -l)
 WIFI_STAT=$(ping -q -c3 8.8.8.8 | awk '/received/ {print $4}')
 NUPLDF=$(ls /home/pi/behavioral_profiling/Documents/logs/ | wc -l)
 
