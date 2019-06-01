9186582961e4ca97a3db65920df2397c93a9a9f5
Ashraf Shaikh Mohammed <ashraf@Ashrafs-MacBook-Pro.local>
2019-06-01 12:57:20 -0500

    Ready for final test

--- final_logger_v2.py
+++ final_logger_v2.py
@@ -208,11 +208,8 @@ try:
         distance_total += distance_interval
         if time2 == time1:
             acceleration = 0.0
-            #print('acceleration: ' + str(acceleration))
         else:
             acceleration = (vspeed2 - vspeed1)/(time2 - time1)
-            #print('vspeed1: ' + str(vspeed1) + ', vspeed2: ' + str(vspeed2) + ', acceleration: ' + str(acceleration))
-            #print('v2 - v1: ' + str(vspeed2 - vspeed1) + ', t2 - t1: ' + str(time2 - time1))
         #time_interval = total_time2 - total_time1
         time_interval = time2 - time1
         total_time += time_interval
